e331b7858f08dfde0d8b9e76f3c747d4828e6bee
35007 <lei24627@gtiit.edu.cn>
2026-04-23 00:28:56 +0800

    update tests

--- Interpreter2.hs
+++ Interpreter2.hs
@@ -24,21 +24,21 @@ executeStmtWithLog stmt mem = case stmt of
   StmtSkip -> ([], mem)
   StmtAssign name expr -> ([], storeMem name (evalExpr mem expr) mem)
   StmtWrite expr -> ([evalExpr mem expr], mem)
-  StmtSequence stmtone stmttwo -> (log1++log2,mem2)
+  StmtSequence stmtone stmttwo -> (log1 ++ log2, mem2)
     where
-      (log1,mem1)=executeStmtWithLog stmtone mem
-      (log2,mem2)=executeStmtWithLog stmttwo mem1
+      (log1, mem1) = executeStmtWithLog stmtone mem
+      (log2, mem2) = executeStmtWithLog stmttwo mem1
   StmtIf expr stmtone stmttwo ->
     if evalExpr mem expr == VBool True
       then executeStmtWithLog stmtone mem
       else executeStmtWithLog stmttwo mem
   StmtWhile expr stmt ->
     if evalExpr mem expr == VBool True
-      then (log1++log2,mem2)
+      then (log1 ++ log2, mem2)
       else ([], mem)
-      where
-          (log1,mem1)=executeStmtWithLog stmt mem
-          (log2,mem2)=executeStmtWithLog (StmtWhile expr stmt) mem1
+    where
+      (log1, mem1) = executeStmtWithLog stmt mem
+      (log2, mem2) = executeStmtWithLog (StmtWhile expr stmt) mem1
 
 --------------------------------------------------------------------------------
 
@@ -68,43 +68,51 @@ test0 :: Bool
 test0 = finalOutputLog == expectedValue
   where
     initialMem = storeMem "n" (VInteger 5) emptyMem
-    finalOutputLog  = fst (executeStmtWithLog countToN initialMem)
+    finalOutputLog = fst (executeStmtWithLog countToN initialMem)
     expectedValue = [VInteger 0, VInteger 1, VInteger 2, VInteger 3, VInteger 4]
 
 test1 :: Bool
 test1 = finalOutputLog == expectedValue
   where
-    -- Program: write(1); write(2); write(3)
-    stmt = StmtSequence (StmtWrite (ExprConst (VInteger 1)))
-                        (StmtSequence (StmtWrite (ExprConst (VInteger 2)))
-                                      (StmtWrite (ExprConst (VInteger 3))))
-    -- 我们不需要特定的初始内存，传一个空的进去即可
+    stmt =
+      StmtSequence
+        (StmtWrite (ExprConst (VInteger 1)))
+        ( StmtSequence
+            (StmtWrite (ExprConst (VInteger 2)))
+            (StmtWrite (ExprConst (VInteger 3)))
+        )
     finalOutputLog = fst (executeStmtWithLog stmt emptyMem)
-    -- 期望严格按顺序输出 1, 2, 3
-    expectedValue  = [VInteger 1, VInteger 2, VInteger 3]
+    expectedValue = [VInteger 1, VInteger 2, VInteger 3]
 
 test2 :: Bool
 test2 = finalOutputLog == expectedValue
   where
-    -- Program: if (1 < 2) { write(1) } else { write(0) }
     cond = ExprBinary OpLessThan (ExprConst (VInteger 1)) (ExprConst (VInteger 2))
-    stmt = StmtIf cond
-                  (StmtWrite (ExprConst (VInteger 1))) -- True branch
-                  (StmtWrite (ExprConst (VInteger 0))) -- False branch
-    
+    stmt =
+      StmtIf
+        cond
+        (StmtWrite (ExprConst (VInteger 1)))
+        (StmtWrite (ExprConst (VInteger 0)))
+
     finalOutputLog = fst (executeStmtWithLog stmt emptyMem)
-    -- 因为 1 < 2 为真，所以只应该输出 1
-    expectedValue  = [VInteger 1]
+    expectedValue = [VInteger 1]
 
 test3 :: Bool
 test3 = finalOutputLog == expectedValue
   where
-    -- Program: if (2 < 1) { write(1) } else { write(0) }
     cond = ExprBinary OpLessThan (ExprConst (VInteger 2)) (ExprConst (VInteger 1))
-    stmt = StmtIf cond
-                  (StmtWrite (ExprConst (VInteger 1))) -- True branch
-                  (StmtWrite (ExprConst (VInteger 0))) -- False branch
-    
+    stmt =
+      StmtSequence
+        ( StmtIf
+            cond
+            (StmtWrite (ExprConst (VInteger 1)))
+            (StmtWrite (ExprConst (VInteger 0)))
+        )
+        ( StmtIf
+            (ExprUnary OpNot cond)
+            (StmtWrite (ExprConst (VInteger 1)))
+            (StmtWrite (ExprConst (VInteger 0)))
+        )
+
     finalOutputLog = fst (executeStmtWithLog stmt emptyMem)
-    -- 因为 2 < 1 为假，所以只应该输出 0
-    expectedValue  = [VInteger 0]
+    expectedValue = [VInteger 0, VInteger 1]
--- Interpreter3.hs
+++ Interpreter3.hs
@@ -75,45 +75,36 @@ test0 = executeStmtWithReturn squareRoot initialMem == expectedResult
     initialMem = storeMem "n" (VInteger 99) emptyMem
     expectedResult = RReturn (VInteger 10)
 
---------------------------------------------------------------------------------
--- Test 1: 验证 Sequence 语句的短路特性 (s1 执行了 return，s2 必须被跳过)
--- 对应文档要求："if s1 returns a value then s2 is not executed"
---------------------------------------------------------------------------------
 test1 :: Bool
 test1 = executeStmtWithReturn stmt emptyMem == expectedResult
   where
-    -- 程序逻辑: return(1); x := 99
     stmt = StmtSequence
              (StmtReturn (ExprConst (VInteger 1)))
              (StmtAssign "x" (ExprConst (VInteger 99)))
-    -- 预期结果: 解释器应当立即返回 1，彻底无视后面的赋值语句
     expectedResult = RReturn (VInteger 1)
 
---------------------------------------------------------------------------------
--- Test 2: 验证 Sequence 语句的正常流转 (s1 不 return，s2 执行并 return)
--- 对应文档要求："if s1 does not return a value then s2 must be executed and it can return a value"
---------------------------------------------------------------------------------
 test2 :: Bool
 test2 = executeStmtWithReturn stmt emptyMem == expectedResult
   where
-    -- 程序逻辑: x := 42; return(x)
     stmt = StmtSequence
              (StmtAssign "x" (ExprConst (VInteger 42)))
              (StmtReturn (ExprVar "x"))
-    -- 预期结果: 前半部分改变了内存，后半部分成功读取内存并返回 42
     expectedResult = RReturn (VInteger 42)
 
---------------------------------------------------------------------------------
--- Test 3: 验证 If 语句分支隔离 (条件为真，屏蔽 else 分支的 return)
--- 对应文档要求："if the condition is true then the return statements in s2 have no effect"
---------------------------------------------------------------------------------
 test3 :: Bool
 test3 = executeStmtWithReturn stmt emptyMem == expectedResult
   where
-    -- 程序逻辑: if (1 < 2) { return(1) } else { return(2) }
     cond = ExprBinary OpLessThan (ExprConst (VInteger 1)) (ExprConst (VInteger 2))
     stmt = StmtIf cond
-             (StmtReturn (ExprConst (VInteger 1))) -- True 分支
-             (StmtReturn (ExprConst (VInteger 2))) -- False 分支
-    -- 预期结果: 因为 1 < 2 恒为真，所以只能返回 1，else 分支里的 2 绝对不能生效
+             (StmtReturn (ExprConst (VInteger 1)))
+             (StmtReturn (ExprConst (VInteger 2)))
     expectedResult = RReturn (VInteger 1)
+
+test4 :: Bool
+test4 = executeStmtWithReturn stmt emptyMem == expectedResult
+  where
+    cond = ExprBinary OpLessThan (ExprConst (VInteger 1)) (ExprConst (VInteger 2))
+    stmt = StmtIf (ExprUnary OpNot cond)
+             (StmtReturn (ExprConst (VInteger 1)))
+             (StmtReturn (ExprConst (VInteger 2)))
+    expectedResult = RReturn (VInteger 2)
